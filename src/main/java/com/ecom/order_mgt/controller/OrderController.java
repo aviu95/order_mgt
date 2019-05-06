fd33d466f05322dee4fd883699adebd083c9f18a
Avinash <avinash@Avinashs-MacBook-Pro.local>
2019-05-06 17:33:15 +0530

    [Avi]Added Default value to Order Request

--- src/main/java/com/ecom/order_mgt/controller/OrderController.java
+++ src/main/java/com/ecom/order_mgt/controller/OrderController.java
@@ -2,11 +2,14 @@ package com.ecom.order_mgt.controller;
 
 import com.ecom.order_mgt.model.dto.OrderRequest;
 import com.ecom.order_mgt.service.OrderService;
+import com.ecom.order_mgt.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.ecom.order_mgt.utils.AppConstants.*;
 
 @RestController
 public class OrderController extends BaseController {
@@ -35,7 +38,10 @@ public class OrderController extends BaseController {
     }
 
     @GetMapping("/orders/user")
-    public ResponseEntity getOrdersWithUser(@RequestParam("pageIndex") Integer page, @RequestParam("size") Integer size, @RequestParam(value = "sort", required = false) String sort) {
+    public ResponseEntity getOrdersWithUser(
+            @RequestParam(value = "pageIndex", defaultValue = PAGE_INDEX_DEFAULT) Integer page,
+            @RequestParam(value = "size", defaultValue = PAGE_SIZE_DEFAULT) Integer size,
+            @RequestParam(value = "sort", required = false) String sort) {
         return ResponseEntity.ok(orderService.getOrderDetails(page, size, sort));
     }
 }
--- src/main/java/com/ecom/order_mgt/utils/AppConstants.java
+++ src/main/java/com/ecom/order_mgt/utils/AppConstants.java
@@ -2,4 +2,6 @@ package com.ecom.order_mgt.utils;
 
 public class AppConstants {
     public final static String USER_CREATED = "USER_CREATED";
+    public final static String PAGE_INDEX_DEFAULT = "0";
+    public final static String PAGE_SIZE_DEFAULT = "10";
 }
